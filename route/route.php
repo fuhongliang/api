67b78a0b5371ee9d7f075a09ebadd293fd38aa20
AdrianLi621 <1772702063@qq.com>
2019-03-15 10:24:42 +0800

    v

--- route/route.php
+++ route/route.php
@@ -11,20 +11,34 @@
 
 
 
-Route::post('member_login','v1/Member/login');//商家登陆
+/**
+ * 后台接口
+*/
+Route::group('', [
+    'member_login'  => 'v1/Member/login',
+    'get_neworder'  => 'v1/Order/newOrder',
+    'refuse_order'  => 'v1/Order/refuseOrder',        //获取团体基本数据
+    'order_list'  => 'v1/Order/getOrderList',
+    'add_goods_class'  => 'v1/Store/addStoreGoodsClass',
+
+]);
 
 
-Route::post('get_neworder','v1/Order/newOrder');//获取新订单
-
-Route::get('refuse_order','v1/Order/refuseOrder');//拒单
-
-
-Route::get('order_list','v1/Order/getOrderList');//根据状态获取订单列表
-
-
-Route::get('add_goods_class','v1/Store/addStoreGoodsClass');//店铺添加商品分类
-
-
+//
+//Route::post('member_login','v1/Member/login');//商家登陆
+//
+//
+//Route::post('get_neworder','v1/Order/newOrder');//获取新订单
+//
+////Route::get('refuse_order','v1/Order/refuseOrder');//拒单
+//
+//
+//Route::get('order_list','v1/Order/getOrderList');//根据状态获取订单列表
+//
+//
+//Route::get('add_goods_class','v1/Store/addStoreGoodsClass');//店铺添加商品分类
+//
+//
 
 
 
