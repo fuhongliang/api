1e96a40cb2e8d16c4d0e29a86ed5f13e03068cdd
AdrianLi621 <1772702063@qq.com>
2019-03-23 17:13:35 +0800

    v

--- application/http/middleware/checkToken.php
+++ application/http/middleware/checkToken.php
@@ -3,19 +3,19 @@
 namespace app\http\middleware;
 use app\v1\controller\Base;
 use app\v1\model\Token as TokenModel;
-use app\v1\model\Token;
+
 
 class checkToken
 {
     public function handle($request, \Closure $next)
     {
 
-        $token=$_SERVER['HTTP_TOKEN'];
+        $token=input('server.HTTP_TOKEN');
         if (!$token)
         {
             return Base::jsonReturn(3000, null, 'token缺失');
         }else{
-            $token=Token::getTokenField(['token'=>$token],['expire_time']);
+            $token=TokenModel::getTokenField(['token'=>$token],['expire_time']);
             if (empty($token))
             {
                 return Base::jsonReturn(3001, null, 'token伪造');
