ead4b11e55a4690afcb5e7e45d77dcbac263eb65
AdrianLi621 <1772702063@qq.com>
2019-03-26 18:31:28 +0800

    v

--- bb.php
+++ bb.php
@@ -1,6 +1,6 @@
 <?php
 
-$cmd = "cd /data/wwwroot/default/hook/api/master &&sudo git pull";
+$cmd = "cd /data/wwwroot/default/hook/api &&sudo git pull";
 $res = shell_exec($cmd);
 var_dump($res);
 exit;
