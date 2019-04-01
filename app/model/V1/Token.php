947d7687da88e48cca6fc9fac0eb306cf15eae0e
“ame <1772702063@qq.com>
2019-04-01 10:09:53 +0800

    v

---
+++ app/model/V1/Token.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\model\V1;
+
+use App\Http\Controllers\BaseController as Base;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Token extends Model
+{ /**
+ * @param $data
+ * @return int|string
+ */
+    static function addToken($data)
+    {
+        return  DB::table('token')->insertGetId($data);
+    }
+
+    /**
+     * @param $condition
+     * @param $field
+     * @return array|\PDOStatement|string|Model|null
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    static function getTokenField($condition,$field)
+    {
+        return DB::table('token')
+            ->where($condition)
+            ->get($field)
+            ->first();
+    }
+
+
+
+}
