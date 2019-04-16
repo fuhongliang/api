ba58ca9d76a80b337f0a032c5ba969ffba4b3ee9
“ame <1772702063@qq.com>
2019-04-16 11:28:34 +0800

    v

--- app/Http/Controllers/SwooleController.php
+++ app/Http/Controllers/SwooleController.php
@@ -9,12 +9,12 @@ use Illuminate\Http\Request;
 class SwooleController
 {
     public function onOpen($server, $fd){
-        echo "建立连接通道ID：{$fd}\n";
+        echo "建立连接通道";
     }
     
     public function onClose($server, $fd){
         //添加警报
-        echo "断开连接通道: {$fd}\n";
+        echo "断开连接通道";
     }
 
     public function onMessage($server, $frame){
