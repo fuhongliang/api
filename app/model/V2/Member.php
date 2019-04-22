364482f6796858001b168857fa220ca8d79669f3
“ame <1772702063@qq.com>
2019-04-22 15:47:42 +0800

    v

--- app/model/V2/Member.php
+++ app/model/V2/Member.php
@@ -2,55 +2,77 @@
 
 namespace App\model\V2;
 
-use Illuminate\Database\Eloquent\Model;
+use App\BModel;
 use Illuminate\Support\Facades\DB;
 
-class Member extends Model
+class Member extends BModel
 {
-    /**
+    /** 获取一条会员数据
      * @param $condition
      * @return mixed
      */
     static function getMemberInfo($condition)
     {
-        return DB::table('member')->where($condition)->first();
+        return BModel::getTableFirstData('member',$condition);
     }
 
+    /** 修改会员信息
+     * @param $condition
+     * @param $up_data
+     * @return int
+     */
     static function editMemberInfo($condition, $up_data)
     {
-        return DB::table('member')
-            ->where($condition)
-            ->update($up_data);
+        return BModel::upTableData('member',$condition,$up_data);
     }
+
+    /** 检查手机号是否存在
+     * @param $condition
+     * @return bool
+     */
     static function checkStorePhoneExist($condition)
     {
-        $count=DB::table('store')
-            ->where($condition)
-            ->count();
-        return $count>0?false:true;
+        $count=BModel::getCount('store',$condition);
+        return $count>0 ? false : true;
     }
+
+    /**检查手机号是否存在
+     * @param $condition
+     * @return bool
+     */
     static function checkStoreJoinPhoneExist($condition)
     {
-        $count=DB::table('store_joinin')
-            ->where($condition)
-            ->count();
+        $count=BModel::getCount('store_joinin',$condition);
         return $count>0?false:true;
     }
+
+    /**检查手机号是否存在
+     * @param $condition
+     * @return bool
+     */
     static function checkStoreRegTmpExist($condition)
     {
-        $count=DB::table('store_register_tmp')
-            ->where($condition)
-            ->count();
+        $count=BModel::getCount('store_register_tmp',$condition);
         return $count>0?false:true;
     }
+
+    /**注册会员信息
+     * @param $data
+     * @return int
+     */
     static function MemberRegister($data)
     {
-        $member_id= DB::table('member')->insertGetId($data);
-        DB::table('member_common')->insert(['member_id'=>$member_id,'auth_modify_pwd_time'=>time()]);
+        $member_id= BModel::insertData('member',$data);
+        BModel::insertData('member_common',['member_id'=>$member_id,'auth_modify_pwd_time'=>time()]);
         return $member_id;
     }
+
+    /**添加临时商家注册记录
+     * @param $data
+     * @return int
+     */
     static function insertMemberRegTmpData($data)
     {
-        return  DB::table('store_register_tmp')->insertGetId($data);
+       return  BModel::insertData('store_register_tmp',$data);
     }
 }
