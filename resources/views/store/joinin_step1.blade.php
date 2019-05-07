0797ca6eaae279d7c62dd4d1d7d40fab056c1416
“ame <1772702063@qq.com>
2019-05-07 18:40:15 +0800

    v

--- resources/views/store/joinin_step1.blade.php
+++ resources/views/store/joinin_step1.blade.php
@@ -4,11 +4,17 @@
     <meta charset=utf-8>
     <title>邻邻发</title>
     <link href="{{asset('static/css/app.87f351d37cd48524312c57a88bbf3cb3.css')}}" rel=stylesheet>
+    <link href={{asset('static/css/app.f8ece47c1df91f47a7871a359f9b6ee1.css')}} rel=stylesheet>
 </head>
 <body>
 <div id=app></div>
 <script type=text/javascript src={{asset('static/js/manifest.3ad1d5771e9b13dbdad2.js')}}></script>
 <script type=text/javascript src={{asset('static/js/vendor.9eb0bab78cccd7ccd405.js')}}></script>
 <script type=text/javascript src={{asset('static/js/app.74a750b4f9798aeafbb7.js')}}></script>
+<script type=text/javascript src={{asset('static/js/manifest.3ad1d5771e9b13dbdad2.js')}}></script>
+<script type=text/javascript src={{asset('static/js/vendor.9eb0bab78cccd7ccd405.js')}}></script>
+<script type=text/javascript src={{asset('static/js/app.d7e02d9330ad08cadc0c.js')}}></script>
 </body>
 </html>
+
+
