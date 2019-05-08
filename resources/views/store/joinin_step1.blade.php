9050db19aa33cca4e00aec85d7413a484e141274
“ame <1772702063@qq.com>
2019-05-08 10:59:50 +0800

    v

--- resources/views/store/joinin_step1.blade.php
+++ resources/views/store/joinin_step1.blade.php
@@ -4,7 +4,7 @@
     <meta charset=utf-8>
     <title>邻邻发</title>
     <link href="{{asset('static/css/app.87f351d37cd48524312c57a88bbf3cb3.css')}}" rel=stylesheet>
-    <link href={{asset('static/css/app.f8ece47c1df91f47a7871a359f9b6ee1.css')}} rel=stylesheet>
+    <link href={{asset('static/css/app.b50895f11093cea40c06455d577a014f.css')}} rel=stylesheet>
 </head>
 <body>
 <div id=app></div>
@@ -12,9 +12,10 @@
 <script type=text/javascript src={{asset('static/js/vendor.9eb0bab78cccd7ccd405.js')}}></script>
 <script type=text/javascript src={{asset('static/js/app.74a750b4f9798aeafbb7.js')}}></script>
 <script type=text/javascript src={{asset('static/js/manifest.3ad1d5771e9b13dbdad2.js')}}></script>
-<script type=text/javascript src={{asset('static/js/vendor.9eb0bab78cccd7ccd405.js')}}></script>
-<script type=text/javascript src={{asset('static/js/app.d7e02d9330ad08cadc0c.js')}}></script>
+<script type=text/javascript src={{asset('static/js/vendor.6621af971e8ba6dcf758.js')}}></script>
+<script type=text/javascript src={{asset('static/js/app.cec590f46b6bbc2f4d7a.js')}}></script>
 </body>
 </html>
 
 
+>
