8ddd5bc03b54185e69ba54d9a65c1920c4b40866
“ame <1772702063@qq.com>
2019-05-14 15:09:00 +0800

    v

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'joininStep1','joininStep2'
+        'store_joinin_step1','store_joinin_step2'
     ];
 }
