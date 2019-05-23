d8d21acd7cbf580e93be64a4fc3c69df56b1d7bd
“ame <1772702063@qq.com>
2019-05-23 14:05:04 +0800

    v

--- routes/u1.php
+++ routes/u1.php
@@ -13,7 +13,6 @@
 Route::post('get_sms', 'BaseController@getSMS');//获取验证码
 
 Route::group(['namespace' => 'U1'], function () {
-
     Route::post('sms_login', 'MemberController@smsLogin');//验证码登录
     Route::post('user_login', 'MemberController@userLogin');//账号密码登录
 });
